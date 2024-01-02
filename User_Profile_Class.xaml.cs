3e813c829014f2b1bb0c31fa449deb7e9616a109
Akram Ahmed Ekram <126765886+Akram14107@users.noreply.github.com>
2024-01-03 01:28:56 +0200

    Create User_Profile_Class.xaml.cs

---
+++ User_Profile_Class.xaml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical_Form
+{
+
+    public class MedicalFormData
+    {
+        public string Mname { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Bdate { get; set; }
+        public string Job { get; set; }
+        public string Hphone { get; set; }
+        public string LastTreatment { get; set; }
+        public string Contact_Way { get; set; }
+        public string Recommended { get; set; }
+
+        public bool AttendingTreatment { get; set; }
+        public bool TakingMedicines { get; set; }
+        public bool Allergic { get; set; }
+        public bool Fever { get; set; }
+        public bool Disease { get; set; }
+        public bool HeartProblems { get; set; }
+        public bool BadReaction { get; set; }
+        public bool HeartSurgery { get; set; }
+        public bool ChestCondition { get; set; }
+        public bool Blackouts { get; set; }
+        public bool Diabetes { get; set; }
+        public bool Bruise { get; set; }
+        public bool Smoke { get; set; }
+
+        // Add other properties for userprofile1 data
+
+
+        // Add other properties for userprofile2 data
+    }
+}
