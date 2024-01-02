f3f152865ddafdb01d1c659777454d29769b549b
Mariamosama300 <155493453+Mariamosama300@users.noreply.github.com>
2024-01-03 01:05:12 +0200

    Add files via upload

---
+++ Book_Appointment.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace Software_Project
+{
+    public partial class Book_Appointment : Window
+    {
+        private const string ConnectionString = "Server=localhost;Database=PROJECT1;User=root;Password=14107;";
+
+        public Book_Appointment()
+        {
+            InitializeComponent();
+        }
+
+        private void BookAppointment_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+
+                    string BookAppointmentQuery = "INSERT INTO appointments (appointment_date, patient_id) " +
+                                   "VALUES (@appointment_date, @patient_id)";
+
+                    using (MySqlCommand BookAppointmentCmd = new MySqlCommand(BookAppointmentQuery, connection))
+                    {
+                        BookAppointmentCmd.Parameters.AddWithValue("@patient_id", txtMedicalRecord.Text);
+                        BookAppointmentCmd.Parameters.AddWithValue("@appointment_date", dpAppointmentDate.SelectedDate);
+
+                        BookAppointmentCmd.ExecuteNonQuery();
+                    }
+                    MessageBox.Show("Data saved successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving data: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ OCR_Page.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Ocsp;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Tesseract;
+
+namespace Software_Project
+{
+    public partial class OCR_Page : Window
+    {
+        string imagepath = @"C:\\Users\\7mada\\Documents\\OCR\\OCR\\img\\pic6.jpg";
+        public OCR_Page()
+        {
+            InitializeComponent();
+            //imageControl.Source = new BitmapImage(new Uri(imagepath));
+        }
+
+        private void PerformOcrButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var engine = new TesseractEngine("./tessData", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(imagepath))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            // Get the recognized text
+                            NewTextBlock.Text = page.GetText();
+                        }
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving data: {ex.Message}");
+            }
+        }
+    }
+}
